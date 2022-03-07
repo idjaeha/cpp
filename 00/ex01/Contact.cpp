9cf9761614d843044024276f6f815091bfc4d94e
idjaeha <jaehanim1@gmail.com>
2022-03-07 18:12:54 +0900

    cpp00 ex01 Contact 클래스 추가

--- 00/ex01/Contact.cpp
+++ 00/ex01/Contact.cpp
@@ -25,3 +25,28 @@ void Contact::setDarkestSecret(std::string darkestSecret)
 {
 	this->darkestSecret = darkestSecret;
 }
+
+std::string Contact::getFirstName()
+{
+	return firstName;
+}
+
+std::string Contact::getLastName()
+{
+	return lastName;
+}
+
+std::string Contact::getNickName()
+{
+	return nickName;
+}
+
+std::string Contact::getPhoneNumber()
+{
+	return phoneNumber;
+}
+
+std::string Contact::getDarkestSecret()
+{
+	return darkestSecret;
+}
--- 00/ex01/Contact.hpp
+++ 00/ex01/Contact.hpp
@@ -20,4 +20,10 @@ public:
 	void setNickName(std::string nickName);
 	void setPhoneNumber(std::string phoneNumber);
 	void setDarkestSecret(std::string darkestSecret);
+
+	std::string getFirstName();
+	std::string getLastName();
+	std::string getNickName();
+	std::string getPhoneNumber();
+	std::string getDarkestSecret();
 };
