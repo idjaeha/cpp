82d76dcc2e4629253457e2298c9e7cfed2765b2e
idjaeha <jaehanim1@gmail.com>
2022-04-19 21:29:17 +0900

    cpp08/ex00 구현 완료

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jayi <jayi@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 02:04:00 by jayi              #+#    #+#             */
+/*   Updated: 2022/04/19 21:28:57 by jayi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_H
+#define EASYFIND_H
+
+#include <algorithm>
+
+class NotFoundException : public std::exception {
+    virtual const char *what(void) const throw() { return "number not fonud."; }
+};
+
+template <typename T> typename T::iterator easyfind(T &container, int num) {
+    typename T::iterator iter =
+        std::find(container.begin(), container.end(), num);
+    if (iter == container.end())
+        throw NotFoundException();
+    return iter;
+}
+
+#endif
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jayi <jayi@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 01:15:41 by jayi              #+#    #+#             */
+/*   Updated: 2022/04/19 21:28:45 by jayi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+
+int main(void) {
+    std::vector<int> vec;
+    std::vector<int>::iterator iter;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+
+    iter = ::easyfind(vec, 1);
+    std::cout << *iter << std::endl;
+    try {
+        iter = ::easyfind(vec, 4);
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
